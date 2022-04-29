3403e9d0d50c3def944550f8ecb403b3647f0146
Aaron Harris <aaronmitchellharris@gmail.com>
2022-04-28 21:38:36 -0700

    Create Question Model

---
+++ server/model/question.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const questionSchema = new mongoose.Schema({
+    prompt: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    choices: {
+        type: map
+    },
+    answer: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model("Question", userSchema);
