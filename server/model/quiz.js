8ca0919417fda8879821744d121617864e842116
Aaron Harris <aaronmitchellharris@gmail.com>
2022-04-28 21:26:27 -0700

    Create Quiz Model

---
+++ server/model/quiz.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const quizSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 255
+  },
+  questions: {
+    type: [ObjectId]
+  }
+});
+
+module.exports = mongoose.model("Quiz", userSchema);
